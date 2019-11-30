58e5742932002576a42467adf10ad95577825b1f
Marcelo Tedeschi <marceloftedeschi@gmail.com>
2019-11-30 16:18:10 +0100

    add custom hyper-apps

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,17 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'a', 'Finder' },
+  { 'c', 'Visual Studio Code' },
+  { 'g', 'Google Chrome' },
+  { 'f', 'Franz' },
+  { 'n', 'Notes' },
+  { 'p', 'Postico' },
+  { 'r', 'Reminders' },
+  { 's', 'Spotify' },
+  { 't', 'iTerm' },
+  { 'y', 'Typora'},
+}
